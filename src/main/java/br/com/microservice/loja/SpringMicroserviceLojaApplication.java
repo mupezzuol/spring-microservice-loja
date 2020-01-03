ff67e9cce51dc292c8b59e45a8126b8799d8d356
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-03 14:24:00 -0300

    Upload base project

---
+++ src/main/java/br/com/microservice/loja/SpringMicroserviceLojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.loja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceLojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceLojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservice/loja/SpringMicroserviceLojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservice.loja;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceLojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
