fabf14122b42b0e543787b9a9de2f3dc6b16a085
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-13 17:36:21 -0300

    Add Interceptor for Feign for Authentication Token

--- src/main/java/br/com/microservice/loja/SpringMicroserviceLojaApplication.java
+++ src/main/java/br/com/microservice/loja/SpringMicroserviceLojaApplication.java
@@ -6,14 +6,38 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.client.RestTemplate;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableResourceServer
 public class SpringMicroserviceLojaApplication {
+	
+	// Add config to intercept Feign requests for when we call another microservices to be passed the authentication token
+	// Add config in file 'application.yml'-> hystrix.shareSecurityContext: true
+	@Bean
+	public RequestInterceptor getInterceptorDeAutenticacao() {
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate template) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if(authentication == null) {
+					return;
+				}
+				
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)authentication.getDetails();
+				template.header("Authorization", "Bearer" + details.getTokenValue());
+			}
+		};
+	}
 	
 	@Bean
 	@LoadBalanced //he will do the intelligence needed to use application names in eureka
